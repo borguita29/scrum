ded667f652c79d4c6c6db7fd927d72ba2b34302d
Dominic Borguita <borguitadominic@gmail.com>
2021-02-12 23:58:16 +0800

    DOM DASHBOARD TEST

--- user/index.php
+++ user/index.php
@@ -2,172 +2,152 @@
     require_once('session.php');
     require_once('header.php');
 ?>
-    
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-xl-12">
-                <div class="col-sm-12" id="divCostumMax">
-                    <div class="row position-relative ">
-                       <img src="../asset/images/bgIndex.jpg" id="divCostumMax" class="imgHolder" style="height: 100vh; width:100vw; filter: brightness(50%);">
-                       <div style="position: absolute;"> 
-                            <div class="row">
-                                <div class="col-xl-1 text-white sideBar " style="background-color: black; opacity: 0.8;">
-                                    <div class="row">
-                                        <div class="userContainer col-xl-12 text-white mt-5 d-flex align-items-center justify-content-center" style="height: 6rem; border: 1px solid white;">
-                                            <div class="sidebarItem">
-                                                A
-                                            </div>
-                                        </div>
-                                        <div class="col-xl-12 text-white mt-2 d-flex align-items-center justify-content-center" style="height: 6rem; border: 1px solid white;">
-                                            <div class="sidebarItem">
-                                                B
-                                            </div>
-                                        </div>
-                                        <div class="col-xl-12 text-white mt-2 d-flex align-items-center justify-content-center" style="height: 6rem; border: 1px solid white;">
-                                            <div class="sidebarItem">
-                                                C
-                                            </div>
-                                        </div>
-                                        <div class="col-xl-12 text-white mt-2 d-flex align-items-center justify-content-center" style="height: 6rem; border: 1px solid white;">
-                                            <div class="sidebarItem">
-                                                D
-                                            </div>
-                                        </div>
-                                        <div class="col-xl-12 text-white mt-2 d-flex align-items-center justify-content-center" style="height: 6rem; border: 1px solid white;">
-                                            <div class="sidebarItem">
-                                                E
-                                            </div>
-                                        </div>
-                                        <div class="col-xl-12 text-white mt-2 d-flex align-items-center justify-content-center" style="height: 6rem; border: 1px solid white;">
-                                            <div class="sidebarItem">
-                                                F
-                                            </div>
-                                        </div>
-                                        <div class="col-xl-12 text-white mt-2 d-flex align-items-center justify-content-center" style="height: 6rem; border: 1px solid white;">
-                                            <div class="sidebarItem">
-                                                G
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-xl-11 text-white contentArea" style="height:100vh; width:100vw;">
-                                    <div class="col-xl-12">
-                                        <div class="row">
-                                            <div class="col-xl-12">
-                                                <div class="row">
-                                                    <div class="col-xl-5 offset-xl-1 mt-4 mr-1" style="background-color: black; opacity: 0.8;  border: 1px solid white;">
-                                                        a
-                                                    </div>
-                                                    <div class="col-xl-5 mt-4 ml-2" style="background-color: black; opacity: 0.8; height: 30vh; .90; border: 1px solid white;">
-                                                        b
-                                                    </div>
-                                                </div>
-                                            </div>
-                                    
-                                            <div class="col-xl-12">
-                                                <div class="row">
-                                                    <div class="col-xl-10 mt-3 offset-xl-1"  style="background-color: black; opacity: 0.8; border: 1px solid white;">
-                                                        <table class="table">
-                                                            <thead class="thead-dark">
-                                                                <tr>
-                                                                <th class="text-white" scope="col">#</th>
-                                                                <th class="text-white" scope="col">First</th>
-                                                                <th class="text-white" scope="col">Last</th>
-                                                                <th class="text-white" scope="col">Handle</th>
-                                                                </tr>
-                                                            </thead>
-                                                            <tbody>
-                                                                <tr>
-                                                                <th class="text-white" scope="row">1</th>
-                                                                <td class="text-white">Mark</td>
-                                                                <td class="text-white">Otto</td>
-                                                                <td class="text-white">@mdo</td>
-                                                                </tr>
-                                                                <tr>
-                                                                <th class="text-white" scope="row">2</th>
-                                                                <td class="text-white">Jacob</td>
-                                                                <td class="text-white">Thornton</td>
-                                                                <td class="text-white">@fat</td>
-                                                                </tr>
-                                                                <tr>
-                                                                <th class="text-white" scope="row">3</th>
-                                                                <td class="text-white">Larry</td>
-                                                                <td class="text-white">the Bird</td>
-                                                                <td class="text-white">@twitter</td>
-                                                                </tr>
-                                                            </tbody>
-                                                        </table>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="col-xl-12">
-                                                <div class="row">
-                                                    <div class="col-xl-10 mt-3 offset-xl-1"  style="background-color: black; opacity: 0.8; border: 1px solid white;">
-                                                        <table class="table">
-                                                            <thead class="thead-dark">
-                                                                <tr>
-                                                                <th class="text-white" scope="col">#</th>
-                                                                <th class="text-white" scope="col">First</th>
-                                                                <th class="text-white" scope="col">Last</th>
-                                                                <th class="text-white" scope="col">Handle</th>
-                                                                </tr>
-                                                            </thead>
-                                                            <tbody>
-                                                                <tr>
-                                                                <th class="text-white" scope="row">1</th>
-                                                                <td class="text-white">Mark</td>
-                                                                <td class="text-white">Otto</td>
-                                                                <td class="text-white">@mdo</td>
-                                                                </tr>
-                                                                <tr>
-                                                                <th class="text-white" scope="row">2</th>
-                                                                <td class="text-white">Jacob</td>
-                                                                <td class="text-white">Thornton</td>
-                                                                <td class="text-white">@fat</td>
-                                                                </tr>
-                                                                <tr>
-                                                                <th class="text-white" scope="row">3</th>
-                                                                <td class="text-white">Larry</td>
-                                                                <td class="text-white">the Bird</td>
-                                                                <td class="text-white">@twitter</td>
-                                                                </tr>
-                                                            </tbody>
-                                                        </table>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-xl-2 text-white userArea" style="background-color: black; opacity: 0.8; height:100vh; width:100vw;">
-                                    User Info
-                                </div>    
-                             </div>
-                        </div>
-                    </div>
-                </div>
+
+<div class="wrapper">
+    <nav id="sidebar">
+
+    <div class="sidebar-header">
+        <h1>SCRUM IO</h1>
+    </div>
+
+    <ul class="lisst-unstyled components">
+        <li class="active">
+            <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">HOME</a> 
+            <ul class="collapse lisst-unstyled" id="homeSubmenu">
+                <li>
+                <a href="#">DATA 1</a>
+                </li>
+                <li>
+                <a href="#">DATA2 1</a>
+                </li>
+            </ul>    
+        </li>
+        <li>
+            <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">PAGES</a> 
+            <ul class="collapse lisst-unstyled" id="pageSubmenu">
+                <li>
+                <a href="#">Page 1</a>
+                </li>
+                <li>
+                <a href="#">Page 2</a>
+                </li>
+            </ul>    
+        </li>
+    </ul>
+
+    </nav>
+
+
+    <div id="content">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <button type="button" id="sidebarCollapse" class="btn btn-info">
+                <i class="fas fa-align-left"></i>
+                <span>CLICK</span>
+            </button>    
+        </div>
+        
+        </nav>
+        <br><br>
+        
+        <div class="card-group mb-5">
+            
+        <div class="card border-primary mb-3" style="max-width: 18rem;">
+            <div class="card-header bg-success">Header</div>
+            <div class="card-body text-primary">
+                <h5 class="card-title">Primary card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
         </div>
+        
+        <div class="card border-primary mb-3" style="max-width: 18rem;">
+        <div class="card-header bg-success">Header</div>
+        <div class="card-body text-primary">
+            <h5 class="card-title">Primary card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+        </div>
+        
+        <div class="card border-primary mb-3" style="max-width: 18rem;">
+        <div class="card-header bg-success">Header</div>
+        <div class="card-body text-primary">
+            <h5 class="card-title">Primary card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+        </div>
+        
+
+
+        </div>
+
+        <div class="card-group">
+            <div class="card">
+                <div class="card-body p-0">
+                    <div class="card-header bg-info">RESERVATION DATA</div>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur aperiam nesciunt, ab ex dolore autem aliquid nobis? Tenetur ipsum, fugiat, ad natus maiores dicta alias culpa cum, dolorum distinctio et.
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur aperiam nesciunt, ab ex dolore autem aliquid nobis? Tenetur ipsum, fugiat, ad natus maiores dicta alias culpa cum, dolorum distinctio et.
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body p-0">
+                    <div class="card-header bg-success">TO DO LIST</div>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur aperiam nesciunt, ab ex dolore autem aliquid nobis? Tenetur ipsum, fugiat, ad natus maiores dicta alias culpa cum, dolorum distinctio et.
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body p-0">
+                    <div class="card-header bg-warning">CLIENTS</div>
+                   <div class="text-center">
+                   <p>USER 1</p>
+                   <p>USER 1</p>
+                   <p>USER 1</p>
+                   </div>
+                </div>
+            </div>
+           
+        </div>
+        <div class="card-group mt-5">
+            <div class="card bg-info">
+                <img class="card-img-top" data-src="holder.js/100x180/" alt="">
+                <div class="card-body">
+                    <h4 class="card-title">GRAPH 1</h4>
+                    <p class="card-text"></p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum ipsum aliquam nihil autem impedit et, culpa dolorum consequatur ipsa inventore facilis commodi illo in non molestiae quo. Eligendi, molestias nihil.
+                </div>
+            </div>
+            <div class="card bg-success">
+                <img class="card-img-top" data-src="holder.js/100x180/" alt="">
+                <div class="card-body">
+                    <h4 class="card-title">GRAPH 2</h4>
+                    <p class="card-text">CONTENT</p>
+                </div>
+            </div>
+            <div class="card bg-primary">
+                <img class="card-img-top" data-src="holder.js/100x180/" alt="">
+                <div class="card-body">
+                    <h4 class="card-title">GRAPH 3</h4>
+                    <p class="card-text">Text</p>
+                </div>
+            </div>
+        
+        </div>
+
+
     </div>
+</div>
+
+
+
+ 
 <?php
     require_once('../footer.php');
 ?>
 <script>
-    $(document).ready(function(){
-        $(".userArea").fadeOut();
-        $(".userContainer").click(function(){
-            $('.contentArea').toggleClass('col-xl-11');
-            $('.contentArea').toggleClass('col-xl-9');
-            if($(".contentArea").hasClass('col-xl-9')){
-                $(".userArea").fadeIn();
-            }
+
+$(document).ready(function () {
+            $('#sidebarCollapse').on('click', function () {
+                $('#sidebar').toggleClass('active');
+            });
         });
-        $(".contentArea").click(function(){
-            $('.contentArea').removeClass('col-xl-9');
-            $('.contentArea').addClass('col-xl-11');
-            if($(".contentArea").hasClass('col-xl-11')){
-                $(".userArea").fadeOut();
-            }
-        });
-    });
+
 </script>
